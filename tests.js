b48d94fb742a8d40349a48164e6522abdaad0690
Oscar Pachano <opachano@gmail.com>
2018-11-11 23:51:41 -0500

    Some JavaScript behaviour tests

---
+++ tests.js
@@ -0,0 +1,28 @@
+let a = [1,2,3]
+console.log(true + a);
+
+var exampleB = function(arg1) {
+  return arg1;
+    };
+
+function example(arg) {
+  arg = "test!";
+  console.log(arg)
+      }
+
+  var x = 1;
+  var y = "example";
+  var z = true;
+
+
+console.log(x);
+
+function plus(amount){
+        return function divide(number){
+          return number / amount;
+        };
+      }
+
+      var f = plus(20);
+
+console.log(f(100));
