3a30eabcff4700f3444987157557277ca3a28456
Oscar Pachano <opachano@gmail.com>
2018-11-19 20:31:04 -0500

    Second interview practice

---
+++ pairCounter.js
@@ -0,0 +1,18 @@
+function sockMerchant(n, ar) {
+  let pairs = 0;
+  for (let i = 0; i < ar.length; i++) {
+      for (let j = i + 1; j < ar.length; j++) {
+          if (ar[i] === ar[j] && ar[i] !== undefined) {
+              ar.splice(j, 1);
+              ar.splice(i, 1);
+              i = 0;
+              j = i + 1
+              pairs++
+          }
+          if (ar[i] === ar[j] && ar.length === 2) {
+              pairs++
+          }
+      }
+  }
+  return pairs;
+}
