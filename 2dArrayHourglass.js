198d54fc25c862b8cc0fbb4bc37d885aadb972c5
Oscar Pachano <opachano@gmail.com>
2018-11-20 20:12:52 -0500

    Started with Array interview questions

---
+++ 2dArrayHourglass.js
@@ -0,0 +1,11 @@
+function hourglassSum(arr) {
+  let result = (arr[0][0] + arr[0][1] + arr[0][2] + arr[1][1] + arr[2][0] + arr[2][1] + arr[2][2]);
+  for (let i = 0; i < arr.length - 2; i++) {
+      for (let j = 0; j < arr[i].length - 2; j++) {
+          if ((arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]) >= result) {
+              result = (arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]);
+          }
+      }
+  }
+  return result
+}
