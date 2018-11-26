682674e9d04c2f2ae4d7c37a98fbffe51e22ffb7
Oscar Pachano <opachano@gmail.com>
2018-11-26 10:06:02 -0500

    Adds Best Practice

--- persistenceBugger.js
+++ persistenceBugger.js
@@ -15,3 +15,16 @@ function persistence(num) {
 }
 
 //Best Practice
+
+function persistence(num) {
+  var times = 0;
+  
+  num = num.toString();
+  
+  while (num.length > 1) {
+    times++;
+    num = num.split('').map(Number).reduce((a, b) => a * b).toString();
+  }
+  
+  return times;
+}
