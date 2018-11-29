776f383968972d2df51a22ff1336ebdd2dd37e53
Oscar Pachano <opachano@gmail.com>
2018-11-29 10:08:38 -0500

    Best Practice added

--- playingWithDigits.js
+++ playingWithDigits.js
@@ -10,3 +10,10 @@ function digPow(n, p){
   let result = (math/n)
   if(Number.isInteger(result)){return result} else {return -1}
 }
+
+//Best Practice 
+
+function digPow(n, p) {
+  var x = String(n).split("").reduce((s, d, i) => s + Math.pow(d, p + i), 0)
+  return x % n ? -1 : x / n
+}
