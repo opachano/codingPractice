69737d290f161312ad795edb205064e5ca153db7
Oscar Pachano <opachano@gmail.com>
2018-12-13 11:32:06 -0500

    Adds the best practice code

--- directions.js
+++ directions.js
@@ -33,3 +33,14 @@ function dirReduc(arr){
 
 // Best Practice
 
+function dirReduc(plan) {
+  var opposite = {
+    'NORTH': 'SOUTH', 'EAST': 'WEST', 'SOUTH': 'NORTH', 'WEST': 'EAST'};
+  return plan.reduce(function(dirs, dir){
+      if (dirs[dirs.length - 1] === opposite[dir])
+        dirs.pop();
+      else
+        dirs.push(dir);
+      return dirs;
+    }, []);
+}
