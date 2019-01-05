640daa392a9f50406d1bddcf7db80fcb656e7b9d
Oscar Pachano <opachano@gmail.com>
2019-01-04 23:57:34 -0500

    freeCodeCamp intermediate algorithms N2

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,5 +1,5 @@
 // Intermediate Algorithm Scripting: Sum All Numbers in a Range
-
+//Number 1
 function sumAll(arr) {
   let answer = 0
   if(arr[0] > arr[1]) {
@@ -12,4 +12,21 @@ function sumAll(arr) {
   }
 
   return answer
+}
+
+//Number 2
+function diffArray(arr1, arr2) {
+  var newArr = [];
+  for(let i = 0; i < arr1.length; i++) {
+    if(arr2.indexOf(arr1[i]) === -1) {
+      newArr.push(arr1[i])
+    }
+  }
+  for(let i = 0; i < arr2.length; i++) {
+    if(arr1.indexOf(arr2[i]) === -1) {
+      newArr.push(arr2[i])
+    }
+  }
+  // Same, same; but different.
+  return newArr;
 }
