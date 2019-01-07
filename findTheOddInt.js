414f12dc0c5e0c009a0a1c3d578502b1a9963ed2
Oscar Pachano <opachano@gmail.com>
2019-01-06 23:51:29 -0500

    Codewars Challenge, find odd int

---
+++ findTheOddInt.js
@@ -0,0 +1,19 @@
+//My Code
+function findOdd(A) {
+  let checker = [];
+  for(let i = 0; i < A.length; i++) {
+    if(checker.indexOf(A[i]) !== -1) {
+      checker.splice(checker.indexOf(A[i]), 1)
+      A.splice(i, 1)
+      i=-1
+    }  else {
+      let tempInt = A[i]
+      checker.push(tempInt)
+      A.splice(i, 1)
+      i=-1
+    }
+  }
+  return checker[0];
+}
+
+//Best Practice
